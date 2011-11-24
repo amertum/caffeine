3e59ccd4215fd46750f4c21d0bcbae09e7f03d8a
amertum <amertum@gmail.com>
2011-11-24 18:20:10 +0100

    solve possibles

--- sudoku/src/main/java/com/google/code/caffeine/sudoku/Sudoku.java
+++ sudoku/src/main/java/com/google/code/caffeine/sudoku/Sudoku.java
@@ -10,50 +10,100 @@ import static com.google.common.base.Predicates.notNull;
 import static com.google.common.collect.DiscreteDomains.integers;
 import static com.google.common.collect.Iterables.concat;
 import static com.google.common.collect.Iterables.filter;
+import static com.google.common.collect.Iterables.getOnlyElement;
 import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Ranges.closed;
-import static com.google.common.collect.Sets.*;
+import static com.google.common.collect.Sets.difference;
+import static com.google.common.collect.Sets.newTreeSet;
+import static com.google.common.collect.Sets.union;
 
 public class Sudoku {
 
     public Sudoku(
-            final Table<Integer, Integer, Integer> board
-    ) {
+            final Table<Integer, Integer, Integer> board)
+    {
         this.board = board;
     }
 
-    public void solve() {
-
+    public void solve()
+    {
+        while (solvePossibles() > 0);
     }
 
-    public Set<Integer> getPossibles(int x, int y) {
+    private int solvePossibles() {
+        System.out.println("solve possibles...");
+        int changes = 0;
+
+        for (Table.Cell<Integer, Integer, Integer> cell : this.board.cellSet()) {
+            final Integer rowKey = cell.getRowKey();
+            final Integer columnKey = cell.getColumnKey();
+            final Integer value = cell.getValue();
+
+            if (value == null) {
+                final Set<Integer> possibles = this.getPossibles(columnKey, rowKey);
+                if (possibles.size() == 1) {
+                    changes++;
+                    final Integer possible = getOnlyElement(possibles);
+                    System.out.println("(" + columnKey + ", " + rowKey + ") : " + possible);
+                    this.board.put(rowKey, columnKey, possible);
+                }
+            }
+        }
+
+        System.out.println("solve possibles : " + changes);
+
+        return changes;
+    }
+
+
+    public Set<Integer> getPossibles(
+            int x,
+            int y)
+    {
         final Integer value = this.board.get(x, y);
         if (value != null) {
             return newTreeSet();
             // ? return newTreeSet(Sets.newHashSet(value));
         }
 
-        final Set<Integer> rowValues = newTreeSet(filter(this.getRow(x), notNull()));
+        //System.out.println("(x, y): (" + x + "," + y + ")");
+
+        final Set<Integer> rowValues = newTreeSet(filter(this.getRow(y), notNull()));
+        //System.out.println("rowValues: " + rowValues);
         final Set<Integer> columnValues = newTreeSet(filter(this.getColumn(x), notNull()));
+        //System.out.println("columnValues: " + columnValues);
         final Set<Integer> squareValues = newTreeSet(filter(this.getSquare(this.getSquareIndex(x, y)), notNull()));
+        //System.out.println("squareValues: " + squareValues);
 
         final Set<Integer> impossibles = newTreeSet(union(union(rowValues, columnValues), squareValues));
-        return difference(closed(1, 9).asSet(integers()), impossibles);
+        //System.out.println("impossibles: " + impossibles);
+        final Set<Integer> possibles = difference(closed(1, 9).asSet(integers()), impossibles);
+        //System.out.println("possibles: " + possibles);
+
+        return possibles;
     }
 
-    public int getSquareIndex(int x, int y) {
+    public int getSquareIndex(
+            int x,
+            int y)
+    {
         return ((y - 1) / 3) * 3 + ((x - 1) / 3) + 1;
     }
 
-    public Set<Integer> getImpossibles(int x, int y) {
+    public Set<Integer> getImpossibles(
+            int x,
+            int y)
+    {
         return difference(closed(1, 9).asSet(integers()), getPossibles(x, y));
     }
 
-    public List<Integer> getRow(int index) {
+    public List<Integer> getRow(int index)
+    {
         return newArrayList(this.board.row(index).values());
     }
 
-    public List<Integer> getColumn(int index) {
+    public List<Integer> getColumn(int index)
+    {
         return newArrayList(this.board.column(index).values());
     }
 
@@ -65,16 +115,17 @@ public class Sudoku {
      * 7 8 9     6  7  8    2 2 2   0 1 2
      * </pre>
      */
-    public List<Integer> getSquare(int index) {
+    public List<Integer> getSquare(int index)
+    {
         int x1 = ((index - 1) / 3) * 3 * 9 + ((index - 1) % 3) * 3;
         int y1 = x1 + 3;
-        return newArrayList(concat(
-                newArrayList(this.board.values()).subList(x1, y1),
-                newArrayList(this.board.values()).subList(x1 + 9, y1 + 9),
-                newArrayList(this.board.values()).subList(x1 + 2 * 9, y1 + 2 * 9)));
+        return newArrayList(concat(newArrayList(this.board.values()).subList(x1, y1),
+                                   newArrayList(this.board.values()).subList(x1 + 9, y1 + 9),
+                                   newArrayList(this.board.values()).subList(x1 + 2 * 9, y1 + 2 * 9)));
     }
 
-    public String toString() {
+    public String toString()
+    {
         String s = "";
 
         for (final Integer rowKey : board.rowKeySet()) {
--- sudoku/src/test/java/com/google/code/caffeine/sudoku/SudokuTest.java
+++ sudoku/src/test/java/com/google/code/caffeine/sudoku/SudokuTest.java
@@ -1,9 +1,7 @@
 package com.google.code.caffeine.sudoku;
 
-import com.google.common.collect.*;
+import com.google.common.collect.ArrayTable;
 import org.junit.Test;
-
-import java.util.List;
 
 import static com.google.common.collect.ArrayTable.create;
 import static com.google.common.collect.DiscreteDomains.integers;
@@ -14,31 +12,9 @@ import static org.fest.assertions.Assertions.assertThat;
 public class SudokuTest {
 
     @Test
-    public void testSolve() throws Exception {
-        final int[][] array =
-                {
-                        {4, 3, 8,   7, 6, 0,   1, 0, 2},
-                        {2, 0, 0,   0, 9, 0,   5, 3, 0},
-                        {0, 0, 0,   0, 0, 2,   6, 0, 8},
+    public void testBoard() throws Exception {
+        final Sudoku sudoku = new Sudoku(makeTable(HARD));
 
-                        {0, 0, 4,   0, 2, 3,   0, 5, 0},
-                        {3, 0, 0,   0, 0, 0,   8, 0, 0},
-                        {6, 0, 0,   0, 0, 0,   0, 0, 0},
-
-                        {0, 0, 5,   0, 1, 0,   3, 0, 9},
-                        {0, 1, 0,   0, 0, 0,   0, 8, 0},
-                        {9, 0, 0,   6, 0, 0,   0, 7, 0},
-                };
-
-        final ArrayTable<Integer, Integer, Integer> board = create(newArrayList(closed(1, 9).asSet(integers())), newArrayList(closed(1, 9).asSet(integers())));
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                final Integer value = (array[i][j] == 0) ? null : array[i][j];
-                board.set(i, j, value);
-            }
-        }
-
-        final Sudoku sudoku = new Sudoku(board);
         assertThat(sudoku.getRow(3)).containsExactly(null, null, null, null, null, 2, 6, null, 8);
         assertThat(sudoku.getColumn(4)).containsExactly(7, null, null, null, null, null, null, null, 6);
         assertThat(sudoku.getSquare(3)).containsExactly(1, null, 2, 5, 3, null, 6, null, 8);
@@ -57,11 +33,73 @@ public class SudokuTest {
         assertThat(sudoku.getImpossibles(2, 2)).containsOnly(1, 2, 3, 4, 5, 8, 9);
 
         System.out.println(sudoku.toString());
+    }
+
+    @Test
+    public void testSolveEasy() throws Exception {
+        final Sudoku sudoku = new Sudoku(makeTable(EASY));
+
+        System.out.println(sudoku.toString());
 
         System.out.println("solving...");
         sudoku.solve();
 
         System.out.println("\n" + sudoku.toString());
     }
+
+    @Test
+    public void testSolveHard() throws Exception {
+        final Sudoku sudoku = new Sudoku(makeTable(HARD));
+
+        System.out.println(sudoku.toString());
+
+        System.out.println("solving...");
+        sudoku.solve();
+
+        System.out.println("\n" + sudoku.toString());
+    }
+
+    private static ArrayTable<Integer, Integer, Integer> makeTable(int[][] array) {
+        final ArrayTable<Integer, Integer, Integer> board = create(newArrayList(closed(1, 9).asSet(integers())), newArrayList(closed(1, 9).asSet(integers())));
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                final Integer value = (array[i][j] == 0) ? null : array[i][j];
+                board.set(i, j, value);
+            }
+        }
+
+        return board;
+    }
+
+    private static final int[][] HARD =
+            {
+                    {4, 3, 8,   7, 6, 0,   1, 0, 2},
+                    {2, 0, 0,   0, 9, 0,   5, 3, 0},
+                    {0, 0, 0,   0, 0, 2,   6, 0, 8},
+
+                    {0, 0, 4,   0, 2, 3,   0, 5, 0},
+                    {3, 0, 0,   0, 0, 0,   8, 0, 0},
+                    {6, 0, 0,   0, 0, 0,   0, 0, 0},
+
+                    {0, 0, 5,   0, 1, 0,   3, 0, 9},
+                    {0, 1, 0,   0, 0, 0,   0, 8, 0},
+                    {9, 0, 0,   6, 0, 0,   0, 7, 0},
+            };
+
+    private static final int[][] EASY =
+            {
+                    {0, 0, 3,   0, 0, 4,   7, 0, 0},
+                    {0, 0, 0,   9, 2, 0,   0, 0, 0},
+                    {8, 0, 0,   0, 0, 5,   9, 0, 2},
+
+                    {6, 7, 0,   0, 0, 0,   0, 8, 3},
+                    {4, 3, 0,   0, 8, 1,   5, 0, 0},
+                    {0, 8, 0,   0, 0, 3,   6, 2, 0},
+
+                    {0, 6, 4,   0, 0, 0,   1, 0, 7},
+                    {1, 0, 0,   0, 4, 0,   2, 0, 0},
+                    {2, 9, 0,   0, 0, 0,   0, 0, 5},
+            };
 
 }
